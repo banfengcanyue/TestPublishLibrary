41d75af6455b742461f48e5b30f415919f0e5607
Wu <banfengcanyue@163.com>
2022-01-21 10:20:57 +0800

    新建库模块，添加发布脚本和注释

--- app/src/main/java/com/bfcy/library/MainActivity.java
+++ app/src/main/java/com/bfcy/library/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bfcy.base.utils.TimeUtil;
+import com.bfcy.common.utils.BaseUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BaseUtils.INSTANCE.getAppName(this);
+        TimeUtil.logTimeZone();
     }
 }
--- mylibrary/src/main/java/com/bfcy/library/TestLibrary.java
+++ mylibrary/src/main/java/com/bfcy/library/TestLibrary.java
@@ -1,5 +1,8 @@
 package com.bfcy.library;
 
+/**
+ * test lib
+ */
 public class TestLibrary {
     public static String getString() {
         return "Hello Library!";
